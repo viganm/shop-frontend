52052765da5cddccb6c0145d9918dead50ac97cb
viganm <vigan.mustafa.vm@gmail.com>
2023-05-03 18:14:13 +0200

    Routing implemented

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -5,17 +5,27 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <ul>
-        <button to="/">Home</button>
-        <button to="/shop">Shop</button>
-        <button to="/about">About</button>
+        <a href="/">
+          <button>Home</button>
+        </a>
+        <a href="/shop">
+          <button>Shop</button>
+        </a>
+        <a href="/about">
+          <button>About</button>
+        </a>
       </ul>
       <div>
-        <button className="shopping-card">
-          <i className="fas fa-shopping-cart"></i>
-        </button>
-        <button to="/login" className="login">
-          Login
-        </button>
+        <a href="/cart">
+          <button href="/shopingCard" className="shopping-card">
+            <i className="fas fa-shopping-cart"></i>
+          </button>
+        </a>
+        <a href="/login">
+          <button href="/login" className="login">
+            Login
+          </button>
+        </a>
       </div>
     </div>
   );
---
+++ src/pages/shop/shop.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+import ItemContainer from "../../components/items/ItemContainer";
+
+const Shop = () => {
+  const [items, setItems] = useState([]);
+
+  const getProducts = async () => {
+    const res = await fetch("http://localhost:3001/products");
+    let items = await res.json();
+    setItems(items.products);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return <ItemContainer products={items} />;
+};
+
+export default Shop;
