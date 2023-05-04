6bfa63ea21bf5b22d7e5c72e39efa6c332a15536
viganm <vigan.mustafa.vm@gmail.com>
2023-05-04 21:41:40 +0200

    new navbar

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -4,27 +4,23 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <ul>
-        <a href="/">
-          <button>Home</button>
-        </a>
-        <a href="/shop">
-          <button>Shop</button>
-        </a>
-        <a href="/about">
-          <button>About</button>
-        </a>
+      <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/shop">Shop</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
       </ul>
-      <div>
-        <a href="/cart">
-          <button href="/shopingCard" className="shopping-card">
-            <i className="fas fa-shopping-cart"></i>
-          </button>
+      <div className="nav-right">
+        <a href="/cart" className="nav-icon">
+          <i className="fas fa-shopping-cart"></i>
         </a>
-        <a href="/login">
-          <button href="/login" className="login">
-            Login
-          </button>
+        <a href="/login" className="nav-button">
+          Login
         </a>
       </div>
     </div>
