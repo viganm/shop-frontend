9428012831feb65005c24fac28d73ec8f787fa61
viganm <vigan.mustafa.vm@gmail.com>
2023-05-04 22:24:51 +0200

    added logo on navbar

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,12 +1,15 @@
 import "./Navbar.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import logo from "../../img/LOGO.jpg";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <ul className="nav-links">
         <li>
-          <a href="/">Home</a>
+          <a href="/">
+            <img src={logo} alt="Logo" />
+          </a>
         </li>
         <li>
           <a href="/shop">Shop</a>
