22ff733158189a3bb3108668d1415535837b867c
viganm <vigan.mustafa.vm@gmail.com>
2023-05-21 23:48:48 +0200

    SHOP-5 login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,5 +1,62 @@
+import React, { useState } from "react";
+import "./Login.css";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const [personEmail, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const loginData = {
+      personEmail,
+      password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (response.ok) {
+        const token = response.headers.get("Authorization");
+        localStorage.setItem("authorization", token);
+        console.log("Login successful!");
+      } else {
+        console.log("Login failed!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={personEmail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default Login;
