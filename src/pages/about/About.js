00f3ec3d63e6113d0e235df9eea52e256385f0c7
viganm <vigan.mustafa.vm@gmail.com>
2023-06-13 20:10:58 +0200

    SHOP-5 About page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,5 +1,25 @@
+import "./About.css";
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className="container">
+      <h1>About</h1>
+      <p>
+        Kimete Mustafa is a highly skilled fashion designer based in Prishtina,
+        Kosovo. With a deep passion for all things fashion, Kimete's designs
+        exude creativity, elegance, and a distinct sense of style. Drawing
+        inspiration from her surroundings and cultural heritage, she seamlessly
+        blends traditional elements with contemporary aesthetics, creating truly
+        unique and captivating pieces. Kimete's meticulous attention to detail
+        and dedication to craftsmanship shine through in every garment she
+        creates. Her designs not only embrace individuality and self-expression
+        but also empower individuals to embrace their own personal style with
+        confidence. With her unwavering commitment to quality and innovation,
+        Kimete Mustafa is redefining fashion in Prishtina and leaving a lasting
+        impression on the industry.
+      </p>
+    </div>
+  );
 };
 
 export default About;
