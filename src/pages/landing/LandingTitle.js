b7e1cb505fa74f6cd77fa6629d179b40b489d280
viganm <vigan.mustafa.vm@gmail.com>
2023-11-01 22:57:02 +0100

    SHOP-3 Landing page video

---
+++ src/pages/landing/LandingTitle.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Landing.css";
+
+const LandingTitle = () => {
+  return <div className="Landing-Title">Enter the Experience</div>;
+};
+
+export default LandingTitle;
